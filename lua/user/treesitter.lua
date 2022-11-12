3bc8182c664d012eb756e2cccc5c891a959b9f87
lisandrojm <lisandrojm@gmail.com>
2022-11-12 16:38:11 -0300

    update

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -3,18 +3,18 @@ if not status_ok then
 	return
 end
 
-local ft_to_parser = require"nvim-treesitter.parsers".filetype_to_parsername
-ft_to_parser.motoko = "typescript"
+-- local ft_to_parser = require"nvim-treesitter.parsers".filetype_to_parsername
+-- ft_to_parser.motoko = "typescript"
 
 configs.setup({
 	ensure_installed = "all", -- one of "all" or a list of languages
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-	ignore_install = { "phpdoc", "php" }, -- List of parsers to ignore installing
+	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
     -- use_languagetree = true,
 		enable = true, -- false will disable the whole extension
 		-- disable = { "css", "html" }, -- list of language that will be disabled
-		disable = { "css", "markdown", "html", "php" }, -- list of language that will be disabled
+		disable = { "css", "markdown"}, -- list of language that will be disabled
 		-- additional_vim_regex_highlighting = true,
 	},
 	autopairs = {

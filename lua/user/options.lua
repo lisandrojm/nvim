5c1ef230a21f54f49cebe288f75474bccf9bb5d2
lisandrojm <lisandrojm@gmail.com>
2022-11-25 08:56:01 -0300

    update

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -20,9 +20,9 @@ local options = {
   splitright = true, -- force all vertical splits to go to the right of current window
   swapfile = false, -- creates a swapfile
   termguicolors = true, -- set term gui colors (most terminals support this)
-  timeoutlen = 100, -- time to wait for a mapped sequence to complete (in milliseconds)
+  timeoutlen = 40, -- time to wait for a mapped sequence to complete (in milliseconds)
   undofile = true, -- enable persistent undo
-  updatetime = 50, -- faster completion (4000ms default)
+  updatetime = 10, -- faster completion (4000ms default)
   writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
   expandtab = true, -- convert tabs to spaces
   shiftwidth = 2, -- the number of spaces inserted for each indentation
@@ -38,6 +38,7 @@ local options = {
   sidescrolloff = 8,
   guifont = "monospace:h17", -- the font used in graphical neovim applications
 }
+
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
